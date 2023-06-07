54031bb344773382e982ccb20583cb02793dee55
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-06-08 00:50:24 +0200

    post and delete method applied

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, []);
+
+    const handleUserDelete = id => {
+        const proceed = window.confirm('Are you sure ?');
+        if (proceed) {
+            console.log('deleting user with id', id);
+            const url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const remaining = users.filter(user => user._id !== id);
+                    }
+                })
+
+        }
+    }
+
     return (
         <div>
-
+            <h2>Available users: {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li key={user._id}>
+                        {user.name}::{user.email}
+                        <button onClick={() => handleUserDelete(user._id)}>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
